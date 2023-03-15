f67fb0edcce5178f0430ab54b7c769e3d3864d6b
mahesh <gangurdemahesh9@gmail.com>
2023-03-15 19:02:55 +0530

    [mahesh]add Program file for UC8

---
+++ Day2_Programming_Constructs/ForLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Programming_Constructs
+{
+    public class ForLoop
+    {
+        //Write a program that takes a user input n and prints a table of the powers
+        //of 2 that are less than or equal to 2^n.
+
+        public void PowerOf2(int number)
+        {
+            for (int i = 0; i <= number; i++)
+            {
+                double result = Math.Pow(2, i);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- Day2_Programming_Constructs/Program.cs
+++ Day2_Programming_Constructs/Program.cs
@@ -11,41 +11,41 @@ namespace Day2_Programming_Constructs
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 2 Practice Problem");
-            
-            //IfElse ifElse = new IfElse();
-            //Console.WriteLine("Enter First value");
-            //int valueOne = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter Second value");
-            //int valueTwo = Convert.ToInt32(Console.ReadLine());
-            //ifElse.EqualorNot(valueOne, valueTwo);
 
-            //ifElse.EvenOrOdd(valueOne);
+            IfElse ifElse = new IfElse();
+            Console.WriteLine("Enter First value");
+            int valueOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second value");
+            int valueTwo = Convert.ToInt32(Console.ReadLine());
+            ifElse.EqualorNot(valueOne, valueTwo);
 
-            //Console.WriteLine("Enter your Age");
-            //int age = Convert.ToInt32(Console.ReadLine());
-            //ifElse.VotingEligiblity(age);
+            ifElse.EvenOrOdd(valueOne);
 
-            //Console.WriteLine("Enter first number ");
-            //int numOne = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter second number ");
-            //int numTwo = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter Third number ");
-            //int numThree = Convert.ToInt32(Console.ReadLine());
-            //ifElse.LargeNumber(numOne, numTwo, numThree);
+            Console.WriteLine("Enter your Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            ifElse.VotingEligiblity(age);
 
-            //Console.WriteLine("Enter your Math Mark");
-            //int mathMark = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter your Physics Mark");
-            //int phyMark = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter your Chemistry Mark");
-            //int chemMark = Convert.ToInt32(Console.ReadLine());
-            //ifElse.AdmissionCriteria(mathMark, phyMark, chemMark);
+            Console.WriteLine("Enter first number ");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number ");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Third number ");
+            int numThree = Convert.ToInt32(Console.ReadLine());
+            ifElse.LargeNumber(numOne, numTwo, numThree);
+
+            Console.WriteLine("Enter your Math Mark");
+            int mathMark = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Physics Mark");
+            int phyMark = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Chemistry Mark");
+            int chemMark = Convert.ToInt32(Console.ReadLine());
+            ifElse.AdmissionCriteria(mathMark, phyMark, chemMark);
 
             ////Switch Statment//
             SwitchStatement switchStatement=new SwitchStatement();
-            //Console.WriteLine("Enter Day Number in Between 1 to 7");
-            //int dayNumber = Convert.ToInt32(Console.ReadLine());
-            //switchStatement.WeekdayName(dayNumber);
+            Console.WriteLine("Enter Day Number in Between 1 to 7");
+            int dayNumber = Convert.ToInt32(Console.ReadLine());
+            switchStatement.WeekdayName(dayNumber);
 
             Console.WriteLine("Enter Number \n 1 for Addition \n 2 for Subtraction \n 3 for Multiplication \n 4 for Division");
             int operationNumber = Convert.ToInt32(Console.ReadLine());
@@ -53,9 +53,14 @@ namespace Day2_Programming_Constructs
             int numberOne = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter second Number");
             int numberTwo = Convert.ToInt32(Console.ReadLine());
-            switchStatement.ArithmaticOperation(operationNumber,numberOne,numberTwo);
+            switchStatement.ArithmaticOperation(operationNumber, numberOne, numberTwo);
 
+            Console.Write("Enter a number for power of 2");
+            int number = Convert.ToInt32(Console.ReadLine());
+            ForLoop forLoop = new ForLoop();
+            forLoop.PowerOf2(number);
 
+           
             Console.ReadLine();
         }
        
