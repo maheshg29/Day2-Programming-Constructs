958664960a20fe88ccd06c16a1416d622c5e7bde
mahesh <gangurdemahesh9@gmail.com>
2023-03-15 18:25:05 +0530

    [mahesh]add program file for UC7

--- Day2_Programming_Constructs/Program.cs
+++ Day2_Programming_Constructs/Program.cs
@@ -12,41 +12,52 @@ namespace Day2_Programming_Constructs
         {
             Console.WriteLine("Welcome to Day 2 Practice Problem");
             
-            IfElse ifElse = new IfElse();
-            Console.WriteLine("Enter First value");
-            int valueOne = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Second value");
-            int valueTwo = Convert.ToInt32(Console.ReadLine());
-            ifElse.EqualorNot(valueOne, valueTwo);
+            //IfElse ifElse = new IfElse();
+            //Console.WriteLine("Enter First value");
+            //int valueOne = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Second value");
+            //int valueTwo = Convert.ToInt32(Console.ReadLine());
+            //ifElse.EqualorNot(valueOne, valueTwo);
 
-            ifElse.EvenOrOdd(valueOne);
+            //ifElse.EvenOrOdd(valueOne);
 
-            Console.WriteLine("Enter your Age");
-            int age = Convert.ToInt32(Console.ReadLine());
-            ifElse.VotingEligiblity(age);
+            //Console.WriteLine("Enter your Age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //ifElse.VotingEligiblity(age);
 
-            Console.WriteLine("Enter first number ");
-            int numOne = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second number ");
-            int numTwo = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Third number ");
-            int numThree = Convert.ToInt32(Console.ReadLine());
-            ifElse.LargeNumber(numOne, numTwo, numThree);
+            //Console.WriteLine("Enter first number ");
+            //int numOne = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter second number ");
+            //int numTwo = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Third number ");
+            //int numThree = Convert.ToInt32(Console.ReadLine());
+            //ifElse.LargeNumber(numOne, numTwo, numThree);
 
-            Console.WriteLine("Enter your Math Mark");
-            int mathMark = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter your Physics Mark");
-            int phyMark = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter your Chemistry Mark");
-            int chemMark = Convert.ToInt32(Console.ReadLine());
-            ifElse.AdmissionCriteria(mathMark, phyMark, chemMark);
+            //Console.WriteLine("Enter your Math Mark");
+            //int mathMark = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your Physics Mark");
+            //int phyMark = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your Chemistry Mark");
+            //int chemMark = Convert.ToInt32(Console.ReadLine());
+            //ifElse.AdmissionCriteria(mathMark, phyMark, chemMark);
 
-            //Switch Statment//
+            ////Switch Statment//
             SwitchStatement switchStatement=new SwitchStatement();
-            Console.WriteLine("Enter Day Number in Between 1 to 7");
-            int dayNumber = Convert.ToInt32(Console.ReadLine());
-            switchStatement.WeekdayName(dayNumber);
+            //Console.WriteLine("Enter Day Number in Between 1 to 7");
+            //int dayNumber = Convert.ToInt32(Console.ReadLine());
+            //switchStatement.WeekdayName(dayNumber);
 
+            Console.WriteLine("Enter Number \n 1 for Addition \n 2 for Subtraction \n 3 for Multiplication \n 4 for Division");
+            int operationNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first Number");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            switchStatement.ArithmaticOperation(operationNumber,numberOne,numberTwo);
+
+
+            Console.ReadLine();
         }
+       
     }
 }
--- Day2_Programming_Constructs/SwitchStatement.cs
+++ Day2_Programming_Constructs/SwitchStatement.cs
@@ -40,5 +40,35 @@ namespace Day2_Programming_Constructs
                     break;  
             }
         }
+
+        //Take 2 numbers has user inputs and perform arithmetic operations on it.
+        // i.e Addition, Subtraction, Multiplication and Division using switch Case.
+        public void ArithmaticOperation(int operationNumber, int numberOne,int numberTwo)
+        {
+            
+            switch (operationNumber)
+            {
+
+                case 1:
+                    int addition = numberOne + numberTwo;
+                    Console.WriteLine("Addition of Given number is " + addition);
+                    break;
+                case 2:
+                    int subtraction = numberOne - numberTwo;
+                    Console.WriteLine("subtraction of Given number is " + subtraction);
+                    break;
+                case 3:
+                    int Multiplication = numberOne * numberTwo;
+                    Console.WriteLine("Multiplication of Given number is " + Multiplication);
+                    break;
+                case 4:
+                    int division = numberOne / numberTwo;
+                    Console.WriteLine("Division of Given number is " + division);
+                    break;
+                default :
+                    Console.WriteLine("Please Enter valid Operation Number ");
+                    break;
+            }
+        }
     }
 }
